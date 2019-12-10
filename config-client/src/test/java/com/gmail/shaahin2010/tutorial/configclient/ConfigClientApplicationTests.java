908c822b1693c2b23889b6ca6343ebcd089dabae
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-12-11 00:23:32 +0330

    add client

---
+++ config-client/src/test/java/com/gmail/shaahin2010/tutorial/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahin2010.tutorial.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
