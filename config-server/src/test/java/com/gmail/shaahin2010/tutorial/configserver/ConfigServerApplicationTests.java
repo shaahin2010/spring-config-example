4fa7db1f0ffd5c63db99c5e28d39aaaf44140a94
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-12-11 00:24:09 +0330

    add config server

---
+++ config-server/src/test/java/com/gmail/shaahin2010/tutorial/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahin2010.tutorial.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
